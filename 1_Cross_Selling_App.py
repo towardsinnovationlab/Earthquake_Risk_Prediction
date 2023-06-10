8edff8980cab6c0970cb8e50dfa18d6f601a890d
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-10 13:29:44 +0200

    Update 1_Cross_Selling_App.py

--- 1_Cross_Selling_App.py
+++ 1_Cross_Selling_App.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.write("# Welcome to the Earthquakes Risk Modelling with Quantile Approach Web App! 👋")
+st.write("# Welcome to the Earthquakes Risk Modelling with Quantile Approach Web App!)
+         
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
+        )
 
 
-st.image("./images/natural-disaster.png", width=500)
+st.image("./images/natural-disaster.png", width=500, pixabay)
