9c11e12a858b2c43a40318ebd3e29e7faab66278
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-10 22:11:32 +0200

    Update 1_App.py

--- 1_App.py
+++ 1_App.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.write("# Welcome to the Earthquakes Risk Modelling with Quantile Approach Web App!")
+st.write("# Welcome to the Web App: Earthquakes Risk Modelling with Quantile Approach")
          
 st.markdown("""
         **👈 Select a page from the dropdown on the left** to see some applications of the app!""")
 
 
 st.image("./images/natural-disaster.png", width=500)
+st.write("Pixabay")
