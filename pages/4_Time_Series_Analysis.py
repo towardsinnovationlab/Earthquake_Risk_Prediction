f71af349660a0aa7b034e2a92355bf9eda5afb3a
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-11 23:32:58 +0200

    Update 4_Time_Series_Analysis.py

--- pages/4_Time_Series_Analysis.py
+++ pages/4_Time_Series_Analysis.py
@@ -27,7 +27,7 @@ st.title("Time Series Analysis")
 
 
 # Formatting features
-df_cleaned.index = pd.to_datetime(df_cleaned.index)
+df_cleaned.time = pd.to_datetime(df_cleaned.time)
 
 df4 = df_cleaned.copy()
 # include date time variables for analysis
