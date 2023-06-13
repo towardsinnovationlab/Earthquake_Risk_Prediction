1bdf506e6ba143650158af07d9bffab3e6797067
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-13 07:22:27 +0200

    Add files via upload

--- pages/5_GBM.py
+++ pages/5_GBM.py
@@ -1,42 +1,21 @@
 #setup deterministic results 
 import numpy as np
-from numpy.random import seed
-seed=0
-np.random.seed(seed)
-import tensorflow as tf
-tf.random.set_seed(seed)
-import os
-os.environ['PYTHONHASHSEED'] = str(seed)
-os.environ['TF_DETERMINISTIC_OPS'] = '1'
 
-
-import random
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import seaborn as sns
 import plotly.express as px
 import datetime
-import scipy
-import scipy.stats as stats
-from scipy.stats import randint
 from statsmodels.tsa.stattools import adfuller
 from pandas.plotting import autocorrelation_plot
 from statsmodels.graphics.tsaplots import plot_acf
-from feature_engine.timeseries.forecasting import WindowFeatures
 from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import TimeSeriesSplit
-#from sklearn_quantile import (
-#    RandomForestQuantileRegressor,
-#    KNeighborsQuantileRegressor)
 from sklearn.ensemble import IsolationForest
 from sklearn.inspection import PartialDependenceDisplay
 from feature_engine.encoding import MeanEncoder
@@ -49,20 +28,14 @@ import warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-st.title("K-Nearest Neighbours model")
+st.title("Gradient Boosting Machine model")
 
 
-#DATA_URL = ('./data/train.csv')
-#df = pd.read_csv(DATA_URL)
-
-#DATA_URL_xtr = ('./data/X_train.csv')
-#X_train = pd.read_csv(DATA_URL_xtr)
-#DATA_URL_xte = ('./data/X_test.csv')
-X_test_sc = pd.read_csv('./data/X_test_sc.csv')
+X_test = pd.read_csv('./data/X_test.csv')
 #DATA_URL_ytr = ('./data/y_train.csv')
 #y_train = pd.read_csv(DATA_URL_ytr)
 #DATA_URL_yte = ('./data/y_test.csv')
-y_test = pd.read_csv('./data/y_test_sc.csv')
+y_test = pd.read_csv('./data/y_test.csv')
 
 
 # vis results
@@ -100,12 +73,11 @@ def boxplot2(data1,data2, var):
 
 # Model
 # loading in the model to predict on the data
-with open('KNN_model.pkl', 'rb') as pickle_in:
-    KNN_regressor = pickle.load(pickle_in)
-    
+with open('GBM_model.pkl', 'rb') as pickle_in:
+    GBM_regressor = pickle.load(pickle_in)
 
 # prediction
-knn_mean_te = KNN_regressor.predict(X_test_sc)
+gbm_mean_te = GBM_regressor.predict(X_test)
 
 
 # Transformations
@@ -113,9 +85,9 @@ scaling = MinMaxScaler()
 #qknn_lower_ = scaling.inverse_transform(np.expm1(qknn_lower_te).reshape(-1,1))
 #qknn_median_ = scaling.inverse_transform(np.expm1(qknn_median_te).reshape(-1,1))
 #qknn_upper_ = scaling.inverse_transform(np.expm1(qknn_upper_te).reshape(-1,1))
-knn_mean_ = scaling.inverse_transform(np.expm1(knn_mean_te).reshape(-1,1))
+gbm_mean_ = np.expm1(gbm_mean_te).reshape(-1,1)
 
-tsmultiplot(y_test, knn_mean_, 'KNN')
+tsmultiplot(y_test, gbm_mean_, 'GBM')
 
-boxplot2(y_test, knn_mean_, 'KNN')
+boxplot2(y_test, gbm_mean_, 'GBM')
 
