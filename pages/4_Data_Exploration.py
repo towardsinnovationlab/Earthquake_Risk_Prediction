fddc05bd0ce64ca1156f1025f8e1174e5900cd3b
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-18 18:42:16 +0200

    Update 4_Data_Exploration.py

--- pages/4_Data_Exploration.py
+++ pages/4_Data_Exploration.py
@@ -87,7 +87,7 @@ df_min
 # Vis Chart
 def plot_target(data, var):
     fig=plt.figure()
-    plt.rcParams['figure.figsize']=(5,5)
+    plt.rcParams['figure.figsize']=(3,5)
     #plt.suptitle('Earthquake Magnitude Exploratory Data Analysis',fontsize=15)
     plt.subplot(1,3,1)
     x=data[var]
