5e69f86c76be72ca24faff2672e6fde97c61c701
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-18 04:40:15 +0200

    Update 2_Introduction.py

--- pages/2_Introduction.py
+++ pages/2_Introduction.py
@@ -2,67 +2,22 @@ import streamlit as st
 
 st.title("Introduction")
 
-st.markdown("""Given an historical time series of Philippines earthquakes from 1979 to 2010, the purpose of this job is to predict the magnitude of 
+st.markdown("""
+
+An earthquake is a natural event that happen when the ground shakes and moves. It is caused by the movement of tectonic plates along faults 
+or fractures in the Earth’s crust when they collide or move against each other.
+An earthquake can release a large amount of energy in the form of seismic waves, which can travel through the Earth and cause damage to buildings, 
+infrastructure, and human lives.
+By understanding the causes and effects of earthquakes, insurers can better assess the risks associated with insuring properties located in areas 
+prone to seismic activity. They can then use this knowledge to set appropriate premiums for their customers, ensuring that those who live in 
+high-risk areas pay higher rates while those who live in low-risk areas pay lower rates.
+
+Given an historical time series of Philippines earthquakes from 1979 to 2010, the purpose of this job is to predict the magnitude of 
 the next earthquakes in the last year by the help of Supervised Learning models.
-Data are coming from a Kaggle hackathon, provided by the United States Geological Survey.
-
-Data have been cleaned into homogeneous magnitude type and having more consistent values, so the data set has been reduced in dimensions (columns and rows) 
-and time horizon.
-
-Columns (index not considered): from 9 to 5
-
-Rows: from 10.188 to 8.999
-
-Time horizion: from 1979-2020 to 1980-2009
-
-### Data Description
-
-#### Variables
-
-**time**
-
-Index of the time series. Time when the event occurred. 
-
-**latitude**
-
-Decimal degrees latitude.
-
-**longitude**
-
-Decimal degrees longitude.
-
-**depth**
-
-The depth where the earthquake begins to rupture.
-
-**magType**
-
-The method or algorithm used to calculate the preferred magnitude for the event.
-
-**place**
-
-Textual description of named geographic region near to the event. This may be a city name, or a Flinn-Engdahl Region name.
-
-**type**
-
-Type of seismic event.
-
-**locationSource**
-
-The network that originally authored the reported location of this event.
-
-**magSource**
-
-Network that originally authored the reported magnitude for this event.
-
-**mag**
-
-Response variable. The magnitude for the event.
-
-#### Evaluation Metric
-
-The evaluation metric used for this data set are MAE and RMSE scores
-
-* Data set source: https://www.kaggle.com/competitions/model-the-impossible-predicting-ph-earthquakes
-
+Magnitude variable is the outcome and it can be used as a feature in a pricing database to improve the quote of the risk premium.
+When we use GLM in a pricing, we estimate the conditional mean of the response variable given the predictor variables. 
+In estimating hazard events like earthquakes become relevant to have a measure of the prediction uncertainty, and the quantile regression solves 
+this issue replacing a single value prediction by prediction intervals. 
+Quantile regression allows for flexible modelling of the entire distribution of the response variable, it estimates the conditional quantiles 
+of the response variable, given the explanatory variables.
 """)
